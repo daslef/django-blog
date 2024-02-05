e7c294cd8e692c7a6f8359e85c25a406e680ba97
Lex Trofimov <daslef93@gmail.com>
2024-02-05 21:18:08 +0300

    blog app created

---
+++ src/blog/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+
+    class Status(models.TextChoices):
+        DRAFT = "DRAFT", "Draft"
+        PUBLISHED = "PUB", "Published"
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=100)
+    body = models.TextField()
+    status = models.CharField(
+        max_length=20, choices=Status.choices, default=Status.DRAFT
+    )
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+
+    published = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-published"]
+        indexes = [models.Index(fields=["-published"])]
+
+    def __str__(self):
+        return self.title
